3ab11de7426ee8f905741ae957cacd87eb6d9643
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-01-27 23:19:06 +0600

    till 029

---
+++ js/03/028.js
@@ -0,0 +1,11 @@
+/*
+--
+--
+028. Introduction to Loops
+--
+https://youtu.be/FJiVQX58yuk?feature=shared
+--
+*/
+// loops are statements that execute some block of code repeatedly until the condition becomes false
+
+// loops and conditions are most fundamental concepts of programming languages
--- js/03/029.js
+++ js/03/029.js
@@ -1,8 +1,16 @@
 /*
 --
 --
-00
+029. JS For Loop
 --
-https://
+https://youtu.be/gDy15jOZQ1Y?feature=shared
 --
 */
+let sum = null;
+for (let i = 0; i < 100; i++) {
+  if (i % 2 === 0) {
+    console.log(sum, '+', i, '=', sum + i);
+    sum += i;
+  }
+}
+console.log(sum);
