8d8d82b05a7c5a3e81a411f288d916ae68d3a34b
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-02-16 19:57:45 +0600

    till 58

---
+++ js/07/058.js
@@ -0,0 +1,16 @@
+/*
+--
+--
+058. What is Function in Javascript 
+--
+https://youtu.be/nfiSlgV_8Yo?feature=shared
+--
+*/
+
+// function === input -- processing -- output
+function greet(name) {
+  console.log('Hello', name);
+}
+let date = new Date();
+console.log(date.getFullYear());
+greet('John'); // Hello John
---
+++ js/07/059.js
@@ -0,0 +1,8 @@
+/*
+--
+--
+059. Defining a Function in Javascript 
+--
+https://www.youtube.com/watch?v=ItJ0qowJ4Hc&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=60
+--
+*/
