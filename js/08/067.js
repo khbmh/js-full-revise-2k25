5c993ff03319e7dbeefe95607e2878f841761ffc
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-02-24 01:04:07 +0600

    till 67

---
+++ js/08/067.js
@@ -0,0 +1,20 @@
+/*
+--
+--
+067. What is Functional Programming in Javascript 
+--
+https://youtu.be/mSESQG-ek8I?feature=shared
+--
+*/
+
+/*
+
+functional programming is a programming paradigm - a style of building the structure and elements of computer programs that treats computation as the evaluation of mathematical functions and avoids changing state and mutable data. - wikipedia
+--- function acts like a value;
+*/
+
+`three main terms`;
+
+// Pure functions
+// first-class functions
+// higher-order functions
---
+++ js/08/068.js
@@ -0,0 +1,8 @@
+/*
+--
+--
+068. Pure Functions in Javascript
+--
+https://www.youtube.com/watch?v=pOcNXZlhhMU&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=69
+--
+*/
