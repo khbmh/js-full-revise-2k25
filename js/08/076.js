4a42e638ca877ff67cf85bc79b54b68ec0d6a0bc
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-02-26 05:14:50 +0600

    reduce

--- js/08/076.js
+++ js/08/076.js
@@ -3,6 +3,30 @@
 --
 076. Reduce Function in Javascript
 --
-https://www.youtube.com/watch?v=YAvJVcVKwu8&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=77
+https://youtu.be/YAvJVcVKwu8?feature=shared
 --
 */
+
+let arr = [1, 2, 4, 3];
+
+let reduced = arr.reduce(function (p, c) {
+  return p + c;
+}, 0);
+
+console.log(reduced);
+
+function reducer(arr, cb, acc) {
+  for (let i = 0; i < arr.length; i++) {
+    acc = cb(acc, arr[i]);
+  }
+  return acc;
+}
+let reducedFun = reducer(
+  arr,
+  function (p, n) {
+    return p * n;
+  },
+  1,
+);
+
+console.log(reducedFun);
