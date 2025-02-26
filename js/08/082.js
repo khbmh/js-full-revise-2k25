240c14fbe8d6ea5437dc6f909ff9379671e0ec3e
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-02-27 01:28:57 +0600

    func composition

--- js/08/082.js
+++ js/08/082.js
@@ -7,3 +7,14 @@ https://youtu.be/KMNNv2hK7S4?feature=shared
 --
 */
 
+// a functions output to be passed as another functions input
+
+function multiply(n) {
+  return n * 5;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(multiply(sum(7, 3)));
