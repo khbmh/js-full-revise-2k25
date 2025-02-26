e5609e1826224b826e5726be46d1ba81060f18ad
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-02-27 01:12:11 +0600

    recursive

--- js/08/080.js
+++ js/08/080.js
@@ -3,7 +3,33 @@
 --
 080. Recursive Function in Javascript 
 --
-https://www.youtube.com/watch?v=5eMlqhgb6i0&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=81
+https://youtu.be/5eMlqhgb6i0?feature=shared
 --
 */
 
+let arr = [1, 2, 3, 4, 5, 6, 7];
+function looper(n) {
+  if (n === 0) {
+    return;
+  }
+  console.log('hello', n);
+  looper(n - 1);
+}
+// looper(9);
+
+function sum(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * sum(n - 1);
+}
+
+// console.log(sum(5));
+
+function arrLoop(arr, lastIndex) {
+  if (lastIndex < 0) {
+    return 0;
+  }
+  return arr[lastIndex] + arrLoop(arr, lastIndex - 1);
+}
+console.log(arrLoop(arr, arr.length - 1));
