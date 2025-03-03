e02a7c7da2afac65785b36ed6bd22365d4f99dd7
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-03-03 09:53:32 +0600

    oop 4 pillars

--- js/10/101.js
+++ js/10/101.js
@@ -3,6 +3,29 @@
 --
 101. Four Pillars of OOP
 --
-https://www.youtube.com/watch?v=ySTFGrrx1IA&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=102
+https://youtu.be/ySTFGrrx1IA?feature=shared
 --
 */
+/*
+
+1. abstraction ==> 
+
+  2. encapsulation ==>
+
+    3. inheritance ==>
+
+      4. polymorphism ==>
+
+*/
+
+const FourPillars_OOP = {
+  Abstraction: `The process of hiding complex implementation details and showing only the essential features of an object. It allows users to interact with objects through simple interfaces, making the code easier to understand and use.`,
+
+  Encapsulation: `The concept of bundling data (variables) and methods (functions) that operate on the data into a single unit (class). It also restricts direct access to some of the object's components, typically by using access modifiers like private or protected, which helps protect the integrity of the data.`,
+
+  Inheritance: `The mechanism by which one class (child) can inherit properties and behaviors (methods) from another class (parent). It promotes code reusability and allows for hierarchical class structures.`,
+
+  Polymorphism: `The ability of different objects to respond to the same method call in their own way. It allows one interface to be used for a general class of actions, making the system more flexible and extensible.`,
+};
+
+console.log(FourPillars_OOP.Encapsulation);
