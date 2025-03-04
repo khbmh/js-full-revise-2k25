030128e68b82f61a0e8df820107332acba353966
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-03-04 17:36:32 +0600

    constructor pattern

--- js/11/110.js
+++ js/11/110.js
@@ -7,3 +7,23 @@ https://youtu.be/DB-dN4TtYD0?feature=shared
 --
 */
 
+const Rectangle = function (width, height) {
+  this.width = width;
+  this.height = height;
+  this.draw = function () {
+    console.log('i am a rectangle');
+    this.properties();
+    console.log(this); // current object
+  };
+  this.properties = function () {
+    console.log('my width is', this.width);
+    console.log('my height is', this.height);
+  };
+};
+
+const rect1 = new Rectangle(22, 55);
+// console.log('hello');
+rect1.draw();
+
+// console.log(rect1);
+console.log(this); // window object
--- js/11/111.js
+++ js/11/111.js
@@ -1,8 +1,8 @@
 /*
 --
 --
-kk
+111. New Keyword in Javascript
 --
-lk
+https://www.youtube.com/watch?v=-OqmWA_P1WI&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=112
 --
 */
