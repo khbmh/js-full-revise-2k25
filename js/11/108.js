d5f90e28c4aef66ed3055bc45b7d91dc24248fa4
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-03-04 17:22:43 +0600

    this in js

--- js/11/108.js
+++ js/11/108.js
@@ -1,8 +1,32 @@
 /*
 --
 --
-kk
+108. "THIS" in Javascript
 --
-lk
+https://youtu.be/ro9x3SVucDY?feature=shared
 --
 */
+
+// object er nijer property mean kore ==> this || otherwise window object
+
+const rect = {
+  width: 50,
+  height: 100,
+  draw: function () {
+    console.log('i am a rectangle');
+    this.printProperties();
+  },
+  printProperties: function () {
+    console.log('my width is', this.width);
+    console.log('my height is', this.height);
+  },
+};
+
+const rect2 = {
+  width: 100,
+  height: 200,
+  draw: rect.printProperties,
+};
+// console.log(r);
+
+rect2.draw();
