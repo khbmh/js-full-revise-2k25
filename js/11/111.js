c71e19b9a76ceb2042d43656a62f5aa24b551c35
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-03-06 02:29:58 +0600

    new keyword

--- js/11/111.js
+++ js/11/111.js
@@ -3,6 +3,37 @@
 --
 111. New Keyword in Javascript
 --
-https://www.youtube.com/watch?v=-OqmWA_P1WI&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=112
+https://youtu.be/-OqmWA_P1WI?feature=shared
 --
 */
+
+// new keyword does three things
+// 1. creates an empty object
+// 2. binds this
+// 3. connects prototypes of the main object and copies it into the newly created empty object
+
+const Rectangle = function (width, height) {
+  this.width = width;
+  this.height = height;
+  this.draw = function () {
+    console.log('i am a rectangle');
+    this.properties();
+    console.log(this); // current object
+  };
+  this.properties = function () {
+    console.log('my width is', this.width);
+    console.log('my height is', this.height);
+  };
+};
+
+function myNew(constructor) {
+  let obj = {};
+  Object.setPrototypeOf(obj, constructor.prototype);
+  var argsArray = Array.prototype.slice.call(arguments);
+  constructor.apply(obj, argsArray.slice(1));
+  return obj;
+}
+
+let rectangle = myNew(Rectangle, 5, 10);
+
+rectangle.draw();
---
+++ js/11/112.js
@@ -0,0 +1,8 @@
+/*
+--
+--
+112. Factory vs Constructor in Javascript
+--
+https://youtu.be/F3k99p89SyY?feature=shared
+--
+*/
